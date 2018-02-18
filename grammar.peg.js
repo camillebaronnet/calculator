0e85b859e824484541a2fd0e3e0c5563e328ff0d
Camille Baronnet <git@camillebaronnet.fr>
2018-02-18 18:44:55 +0100

    Initial commit

---
+++ grammar.peg.js
@@ -0,0 +1,79 @@
+{
+  var floatFix = 1000000000;
+      if(window.exportVars === undefined){
+      window.exportVars = {
+          'pi' : Math.PI
+      };
+    }
+    new Map();
+    //exportVars.set('pi', Math.PI);
+  // https://api.fixer.io/latest
+  function money(value, currency){
+  switch (currency){
+      case 'EUR':
+      return value*1.5;
+    }
+  }
+}
+
+
+Lines = vars:Variable* result:Result {
+  Object.keys(window.exportVars).forEach(function(key){
+      if(window.exportVars[key] === null){
+          window.exportVars[key] = result
+        }
+    });
+  return {
+      'result' : result,
+      'vars' : window.exportVars
+  };
+}
+
+Variable = varname:String _ "=" _ { window.exportVars[varname.join('').toLowerCase()] = null; }
+
+Result = Tolerence exp:Expression Tolerence { return Math.round(exp  * floatFix) / floatFix; }
+
+Expression
+  = head:Term tail:(_ ("+" / "-") _ Term)* {
+      return tail.reduce(function(result, element) {
+        if (element[1] === "+") { return result + element[3]; }
+        if (element[1] === "-") { return result - element[3]; }
+      }, head);
+    }
+
+Term
+  = head:Factor tail:(_ ("*" / "/") _ Factor)* {
+      return tail.reduce(function(result, element) {
+        if (element[1] === "*") { return result * element[3]; }
+        if (element[1] === "/") { return result / element[3]; }
+      }, head);
+    }
+
+Factor
+  = "(" _ expr:Expression _ ")" { return expr; }
+  / CallVariable 
+  / num:Number { return num }
+
+
+CallVariable
+  = string:String { return window.exportVars[string.join('').toLowerCase()]; }
+
+Number "number"
+  = _ [0-9a-fA-F,\.]+ {
+  if(text().match(/[a-fA-F]+/)){
+    return parseInt(text(), 16);
+  }
+  else{
+    return parseFloat(text().replace(',', '.'), 10);
+  }
+}
+
+String = [^0-9 \t\n\r]+
+Word = [a-zA-Z]+ { return text();  }
+Words = (String _)* { return text().split(' ');  }
+
+_ "whitespace"
+  = [ \t]*
+  
+Tolerence = [ \t+()/:=*]*
+              
